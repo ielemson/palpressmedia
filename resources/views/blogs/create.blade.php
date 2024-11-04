3f6ecb8c046c0bb15832d506d44b9842ffad8cc0
Elemson Ifeanyi <ielemson@gmail.com>
2024-11-04 05:33:55 +0100

    updated blog section

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -47,4 +47,19 @@
             </div>
         </div>
     </div>
+
+    @section("js_app")
+    <!-- Place the first <script> tag in your HTML's <head> -->
+<script src="https://cdn.tiny.cloud/1/vr3y2q88fqi6tlc1njmacrchhy2lt40zklrrnv3d8im4jjab/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+
+<!-- Place the following <script> and <textarea> tags your HTML's <body> -->
+<script>
+  tinymce.init({
+    selector: 'textarea',
+    plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+  });
+</script>
+
+@endsection
 @endsection
